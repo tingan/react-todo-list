2927854022f27a79c62ea3b24d750d28f7db1f6a
tingan tang <bend90@gmail.com>
2019-01-08 10:55:30 +0200

    no empty to do list

--- src/App.js
+++ src/App.js
@@ -132,7 +132,7 @@ class App extends Component {
           <button
             onClick={this.state.editing ? this.updateTodo : this.addTodo}
             className="btn-success mb-3 form-control"
-            disabled={this.state.newTodo.length < 5}
+            disabled={this.state.newTodo.trim().length < 1}
           >
             {this.state.editing ? 'Update todo' : 'Add todo'}
           </button>
